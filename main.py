5dc94a6160732d5fc225bc4aed353b663eb931cd
michu3554 <variuxpl@gmail.com>
2021-02-24 13:54:17 +0100

    zad2

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-#zad 1
-a = 1
-b = 2
-c = 1.1
-d = 1.2
-e = 1+1j
-f = 1+2j
-g = "napis"
-h = "napis2"
+# # zad 1
+#
+# a = 1
+# b = 2
+# c = 1.1
+# d = 1.2
+# e = 1+1j
+# f = 1+2j
+# g = "napis"
+# h = "napis2"
+#
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+# print(h)
 
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
-print(f)
-print(g)
-print(h)
+# zad 2
+
+a = 10
+b = 2
+
+dodawanie = a + b
+odejmowanie = a - b
+mnozenie = a * b
+dzielenie = a / b
+
+print(dodawanie)
+print(odejmowanie)
+print(mnozenie)
+print(dzielenie)
